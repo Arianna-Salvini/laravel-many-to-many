be074ccd02ca42f072aa93a94f0f73d332fe3d45
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-27 11:53:22 +0200

    Init

---
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Models\Project;
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('admin.projects.index', ['projects' =>Project::orderByDesc('id')->paginate(7)]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() : View
+    {
+        $types=Type::all();
+
+        return view('admin.projects.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProjectRequest $request): RedirectResponse
+    {
+        //dd($request->all());
+        
+        $validated= $request -> validated();
+        $slug= Str::slug($request->title, '-');
+        $validated['slug'] = $slug;
+
+        if ($request->has('image')){
+        $image_path=Storage::put('uploads', $validated['image']);
+        $validated['image']=$image_path;
+        };
+        // dd($validated, $image_path);
+        // dd($validated);
+        Project::create($validated);
+
+
+
+        return to_route('admin.projects.index')->with('message', 'Project added successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Project $project): View
+    {
+        return view('admin/projects/show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Project $project): View
+    {
+        $types=Type::all();
+        
+        return view('admin.projects.edit', compact('project', 'types'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProjectRequest $request, Project $project): RedirectResponse
+    {
+        // dd($request->all());
+
+        $validated= $request->validated();
+        $slug= Str::slug($request->title, '-');
+        $validated['slug'] = $slug;
+
+        if ($request->has('image')){
+            $image_path=Storage::put('uploads', $validated['image']);
+            $validated['image']=$image_path;
+        }
+
+        $project->update($validated);
+
+        return to_route('admin.projects.show', $project)->with('status', "Project  $project->title updated successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Project $project): RedirectResponse
+    {
+
+        if($project->image){
+            Storage::delete($project->image);
+        };
+
+        $project->delete();
+        
+        return  to_route('admin.projects.index')->with('status', "Project  $project->title DELETED successfully");
+    }
+}
