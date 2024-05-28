429a6d91717c9490b4b094763ee5539d27f99929
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-05-28 09:56:37 +0200

    Fix fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable=['title', 'slug', 'subtitle', 'description', 'image','tecnology' ,'url', 'image', 'type_id'];
+    protected $fillable=['title', 'slug', 'subtitle', 'description', 'image','url', 'type_id'];
 
   /**
    * Get the type that owns the Project
